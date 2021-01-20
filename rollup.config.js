9e50addb09bd119ec2a0baf884f6c8d39158de9f
calimanco <calimanco@qq.com>
2021-01-20 13:48:38 +0800

    chore: update devDependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import json from '@rollup/plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
+import { camelCase } from 'lodash'
 import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
 import pkg from './package.json'
 
 const libraryName = 'promise-polyfill-plus'
@@ -33,7 +33,8 @@ export default {
       tsconfig: 'tsconfig.json',
       tsconfigOverride: {
         compilerOptions: {
-          removeComments: true
+          removeComments: true,
+          module: 'ES2015'
         }
       }
     }),
